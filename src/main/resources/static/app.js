aa3f04ccc4afcb6b461269a70af287f6528ef584
Samuel Moritz <samuel.moritz@gmail.com>
2015-11-12 20:11:46 +0100

    More functionality

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,18 +1,35 @@
 var app = angular.module("noteApp", ['ngMaterial']);
 
-app.controller("NoteController", function($http) {
+app.controller("NoteController", function($http, $mdToast,$document) {
      var vm = this;
 
 
     fetchNotes();
 
+    function toast(msg) {
+        $mdToast.show(
+            $mdToast.simple()
+                .content(msg)
+                .position("top right")
+                .parent($document[0].querySelector("#toastParent"))
+                .hideDelay(2000)
+        );
+    }
+
     vm.createNote = function() {
-        $http.post("/notes",{}).then(fetchNotes)
+        $http.post("/notes",{}).then(function() {
+            toast("Note created");
+            fetchNotes();
+        })
+
     };
 
     vm.updateNote = function() {
-        $http.put("/notes/" + vm.selectedNote.id,vm.selectedNote).then(fetchNotes);
-    }
+        $http.put("/notes/" + vm.selectedNote.id,vm.selectedNote).then(function() {
+            toast("Note updated");
+            fetchNotes();
+        })
+    };
 
     function fetchNotes() {
         $http.get("/notes").then(function (response) {
